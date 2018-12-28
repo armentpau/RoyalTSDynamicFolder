5ac7aa6b38d17cf7f01a321fd98695a6f2c52db6
Paul DeArment <paul.dearmentjr@aurora.org>
2018-12-28 13:10:55 -0600

    Initial Commit

---
+++ RoyalTSDynamicForm/Globals.ps1
@@ -0,0 +1,51 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+$hashPorts = @{
+	"SecureGateway" = 22
+	"RoyalServer" = 54899 
+	"RemoteDesktopGateway" = $null
+	"RemoteDesktopConnection" = 3389
+	"TerminalConnection - SSH" = 22
+	"TerminalConnection - Telnet" = 23
+	"TerminalConnection - Serial" = $null
+	"VNCConnection" = 5900
+	"WindowsEventsConnection" = $null
+	"WindowsServicesConnection" = $null
+	"WindowsProcessesConnection" = $null
+	"TerminalServicesConnection" = $null
+	"PowerShellConnection"	     = $null
+}
+
+function Update-DCTextBoxes
+{
+	if (-not ([string]::IsNullOrEmpty($domainTextBox.Text)))
+	{
+		try
+		{
+			$selectedDomain = Get-ADDomain -Identity $($domainTextBox.text) -ErrorAction Stop
+			$dcRootTextBox.text = $selectedDomain.distinguishedname
+			$dcTextBox.Text = $selectedDomain.pdcemulator
+		}
+		catch
+		{
+			
+		}
+	}
+	else
+	{
+		$dcRootTextBox.Text = ""
+		$dcTextBox.Text = ""
+	}
+}
+
+function Start-showOUDialogs
+{
+	if (-not [string]::IsNullOrEmpty($dcRootTextBox.Text))
+	{
+		$script:currentlySelectedOU = $searchBaseTextBox.text
+		$script:selectedDomainRoot = $dcRootTextBox.text
+		Show-OU_Selector_psf
+		$searchBaseTextBox.Text = $script:selectedOU
+	}
+}
