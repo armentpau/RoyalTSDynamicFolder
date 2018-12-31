a0c0f6a3848584882113c3a644c6335412b4ab5d
Paul DeArment <paul.dearmentjr@aurora.org>
2018-12-31 13:36:57 -0600

    added updates

--- SourceData/RoyalTS/RoyalTSDynamicConnectionFooter.ps1
+++ SourceData/RoyalTS/RoyalTSDynamicConnectionFooter.ps1
@@ -1,5 +1,6 @@
 ﻿Import-Module ActiveDirectory
 [System.Collections.ArrayList]$array = @()
+[System.Collections.ArrayList]$pathArray = @()
 $script:ConfigFilePath = "$($env:APPDATA)\RoyalTSDynamicFolderCustomizer"
 if ("$CustomProperty.ConfigFileName$" -ne '.ConfigFileName$' -and "$CustomProperty.ConfigFileName$" -ne "TODO")
 {
@@ -16,12 +17,7 @@ if (Test-Path "$($script:ConfigFilePath)\$($configFileName)")
 	$searchScope = $data.searchscope
 	$server = $data.server
 	$searchBase = $data.searchbase
-	$credentialName = $data.credentialName
-	$portNumber = $data.portNumber
-	$connectionType = $data.connectionType
-	$useCimChecked = $data.useCimChecked
-	$useConsole = $data.adminConsole
-	if ($useCimChecked -like "Checked")
+	if ($($data.useCimChecked) -like "Checked")
 	{
 		$useCim = "true"
 	}
@@ -29,7 +25,7 @@ if (Test-Path "$($script:ConfigFilePath)\$($configFileName)")
 	{
 		$useCim = "false"
 	}
-	if ($useConsole -like "Checked")
+	if ($($data.adminConsole) -like "Checked")
 	{
 		$useadminConsole = "true"
 	}
@@ -39,56 +35,98 @@ if (Test-Path "$($script:ConfigFilePath)\$($configFileName)")
 	}
 	foreach ($computer in Get-ADComputer -SearchBase $searchBase -Filter $filter -SearchScope $searchScope -Server $server -Properties canonicalname)
 	{
-		if ($connectionType -like "RemoteDesktopConnection")
+		if ($data.useParentCred -eq $true)
 		{
-			$array.add((
-					New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Type" = $connectionType;
-						"Port"			     = $portNumber
-						"Name"			     = $computer.name;
-						"ComputerName"		     = $computer.name;
-						"credentialName"	     = $credentialName;
-						"Path" = $computer.canonicalname.replace("/$($computer.name)", "");
-						"ConsoleSession" = $useadminConsole
-					}
-				)) | Out-Null
+			$subPath = $computer.canonicalname.replace("/$($computer.name)", "")
+			$initialCounter = 0
+			foreach ($pathItem in $subPath.split("/"))
+			{
+				if ($initialCounter -eq 0)
+				{
+					$builderPath = "/"
+					$initialCounter++
+					$previousPath = $pathItem
+					
+					
+				}
+				else
+				{
+					$builderPath = "$($builderPath)/$previousPath".replace('//', '/')
+					$previousPath = $pathItem
+					
+				}
+				if ($pathArray.Contains("$($builderPath)/$pathItem"))
+				{
+					
+				}
+				else
+				{
+					$array.add((
+							New-Object -TypeName System.Management.Automation.PSObject -Property @{
+								"Type"			    = "Folder";
+								"CredentialsFromParent" = "true";
+								"Path"			    = $builderPath;
+								"Name"			    = $pathItem;
+							}
+						)) | Out-Null
+					$pathArray.add("$($builderPath)/$pathItem") | Out-Null
+				}
+			}
 		}
-		elseif ($connectionType -like "TerminalConnection*")
+		if ($($data.connectionType) -like "RemoteDesktopConnection")
 		{
 			$array.add((
 					New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Type" = "TerminalConnection";
-						"TerminalConnectionType" = $connectionType.replace("TerminalConnection - ", "")
-						"Port" = $portNumber
+						"Type" = $data.connectionType;
+						"Port" = $data.portNumber
 						"Name" = $computer.name;
 						"ComputerName" = $computer.name;
-						"credentialName" = $credentialName;
-						"Path" = $computer.canonicalname.replace("/$($computer.name)", "");
+						"credentialName" = $data.credentialName;
+						"Path" = "/" +$computer.canonicalname.replace("/$($computer.name)", "");
+						"ConsoleSession" = $useadminConsole;
+						"CredentialsFromParent" = $data.useParentCred;
 					}
 				)) | Out-Null
 		}
-		elseif ($connectionType -like "Windows*" -or $connectionType -like "TerminalServicesConnection")
+		elseif ($($data.connectionType) -like "TerminalConnection*")
 		{
 			$array.add((
 					New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Type"			     = $connectionType;
+						"Type"			     = "TerminalConnection";
+						"TerminalConnectionType" = $($data.connectionType).replace("TerminalConnection - ", "")
+						"Port"			     = $data.portNumber
+						"Name"			     = $computer.name;
+						"ComputerName"		     = $computer.name;
+						"credentialName"	     = $data.credentialName;
+						"Path"			     = "/" +$computer.canonicalname.replace("/$($computer.name)", "");
+						"CredentialsFromParent"  = $data.useParentCred;
+					}
+				)) | Out-Null
+		}
+		elseif ($($data.connectionType) -like "Windows*" -or $($data.connectionType) -like "TerminalServicesConnection")
+		{
+			$array.add((
+					New-Object -TypeName System.Management.Automation.PSObject -Property @{
+						"Type" = $data.connectionType;
 						"UseCIM" = $useCim;
-						"Name"			     = $computer.name;
-						"ComputerName"		     = $computer.name;
-						"credentialName"	     = $credentialName;
-						"Path"			     = $computer.canonicalname.replace("/$($computer.name)", "");
+						"Name" = $computer.name;
+						"ComputerName" = $computer.name;
+						"credentialName" = $data.credentialName;
+						"Path" = "/" +$computer.canonicalname.replace("/$($computer.name)", "");
+						"CredentialsFromParent" = $data.useParentCred;
 					}
 				)) | Out-Null
 		}
-		elseif ([string]::IsNullOrEmpty($portNumber))
+		elseif ([string]::IsNullOrEmpty($data.portNumber))
 		{
 			$array.add((
 					New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Type" = $connectionType;
+						"Type" = $data.connectionType;
 						"Name" = $computer.name;
 						"ComputerName" = $computer.name;
-						"credentialName" = $credentialName;
-						"Path" = $computer.canonicalname.replace("/$($computer.name)", "");
+						"credentialName" = $data.credentialName;
+						"Path" = "/"+$computer.canonicalname.replace("/$($computer.name)", "");
+						"CredentialsFromParent" = $data.useParentCred;
 					}
 				)) | Out-Null
 		}
@@ -96,19 +134,20 @@ if (Test-Path "$($script:ConfigFilePath)\$($configFileName)")
 		{
 			$array.add((
 					New-Object -TypeName System.Management.Automation.PSObject -Property @{
-						"Type" = $connectionType;
-						"Port" = $portNumber
+						"Type" = $data.connectionType;
+						"Port" = $data.portNumber
 						"Name" = $computer.name;
 						"ComputerName" = $computer.name;
-						"credentialName" = $credentialName;
-						"Path" = $computer.canonicalname.replace("/$($computer.name)", "");
+						"credentialName" = $data.credentialName;
+						"Path" = "/" +$computer.canonicalname.replace("/$($computer.name)", "");
+						"CredentialsFromParent" = $data.useParentCred;
 					}
 				)) | Out-Null
 		}
 		
 	}
-	$array = $array | Sort-Object -Property path
+	$newarray = $array | Sort-Object -Property path
 	$hash = @{ }
-	$hash.add("Objects", $array)
+	$hash.add("Objects", $newarray)
 	$hash | ConvertTo-Json
 }
